3d01b0081dea55b84871046ae9f26d22f9d3eb89
lucasrvasconcelos <lucasvasconcelos900@gmail.com>
2023-08-08 16:04:23 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
 import Express from "express";
 import { engine } from 'express-handlebars';
 import userRoutes from "./src/Routes/userRoutes.js";
-import path from 'path';
-
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-
-const __dirname = path.dirname(__filename);
 
 //configuração express
 const app = Express()
@@ -22,7 +15,7 @@ app.use(Express.static("src/assets"))
 //configuração handlebars
 app.engine('handlebars', engine({defaultLayout: "main.handlebars"}));
 app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', 'views');
 app.enable('view cache');
 
 app.listen(port, () => {
