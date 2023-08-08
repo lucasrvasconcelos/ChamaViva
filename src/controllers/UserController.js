b9cffe0072c4e2db8c49a5d23ae854301341c7dc
lucasrvasconcelos <lucasvasconcelos900@gmail.com>
2023-08-08 13:58:36 -0300

    Update UserController.js

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,7 +3,6 @@ import dados from "../databaseFAKE/dados.js"
 export default class UserControler {
 
     async home(req, res) {
-        console.log(dados.section_01.groupImages[0])
         res.render("home", { dados } )
     } 
 }
