be8cd69b8e6cdee0b0ceae0779c815c356fc020d
lucasrvasconcelos <lucasvasconcelos900@gmail.com>
2023-09-16 15:47:29 -0300

    update to shop

--- src/models/db.js
+++ src/models/db.js
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 import mysql2 from 'mysql2'; // Needed to fix sequelize issues with WebPack
 
-const sequelize = new Sequelize('systeam', 'root', 'crt123456', {
+const sequelize = new Sequelize('systeam', 'root', '1234', {
     host: 'localhost',
     dialectModule: mysql2, // Needed to fix sequelize issues with WebPack
     dialect: 'mysql'
